946aa70a808669bcc9d3b524d3f034bdca6c3d04
Mikhail Fursov <mike.fursov@gmail.com>
2017-12-21 13:24:01 -0500

    Updating dependencies

--- src/main/java/com/github/pesennik/Scripts.java
+++ src/main/java/com/github/pesennik/Scripts.java
@@ -12,7 +12,7 @@ public class Scripts {
 
     public static final HeaderItem PARSLEY_JS = forUrl("https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.0/parsley.min.js", "parsley.js");
 
-    public static final HeaderItem AUTOLINKER_JS = forUrl("https://cdnjs.cloudflare.com/ajax/libs/autolinker/1.4.4/Autolinker.js", "autolinker.js");
+    public static final HeaderItem AUTOLINKER_JS = forUrl("https://cdnjs.cloudflare.com/ajax/libs/autolinker/1.6.0/Autolinker.js", "autolinker.js");
 
     public static final HeaderItem AUTOSIZE_JS = forUrl("https://cdnjs.cloudflare.com/ajax/libs/autosize.js/3.0.20/autosize.min.js", "autosize.js");
 
